423a21caccb33a5f3d70f0eb3083ab672150fd1d
dantasamorim <96582948+dantasamorim@users.noreply.github.com>
2024-01-13 16:27:30 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'vereador1.jpg', legenda: 'vereador' }
+                ]
+
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: 'vereador2.jpg', legenda: 'vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        canditados: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: 'prefeito1.jpg', legenda: 'prefeito' },
+                    { url: 'vice1.jpg', legenda: 'vice-prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERT',
+                vice: 'Zul',
+                fotos: [
+                    { url: 'prefeito2.jpg', legenda: 'prefeito' },
+                    { url: 'vice2.jpg', legenda: 'vice-prefeito', esmall: true }
+                ]
+            },
+
+
+        ]
+    }
+
+];
